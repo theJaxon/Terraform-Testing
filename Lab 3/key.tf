31cde0b4132e8c1f675581056c7240b4f11442c8
Jaxon <mohamedmostels@gmail.com>
2020-04-05 15:38:31 +0200

    jenkinsfile added

---
+++ Lab 3/key.tf
@@ -0,0 +1,48 @@
+/*
+    Create a key on the fly
+    https://www.terraform.io/docs/providers/tls/r/private_key.html
+*/
+
+resource "tls_private_key" "tf_generated_key" {
+  algorithm   = "RSA"
+  ecdsa_curve = "2048"
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "generated_aws_key"
+  public_key = "${tls_private_key.tf_generated_key.public_key_openssh}"
+}
+
+/*
+    Secrets metadata
+*/
+resource "aws_secretsmanager_secret" "tf_key_secretmanager_metadata4" {
+  name = "tf_key_secretmanager_metadata4"
+}
+
+/*
+    Store the private keys in AWS secrets
+*/
+resource "aws_secretsmanager_secret_version" "store_aws_secret" {
+  secret_id     = "${aws_secretsmanager_secret.tf_key_secretmanager_metadata4.id}"
+  secret_string = "${tls_private_key.tf_generated_key.private_key_pem}"
+}
+
+/*
+    Store the private keys offline locally
+*/
+resource "local_file" "aws_secret_local_file" {
+    content     = "public key\n ${tls_private_key.tf_generated_key.public_key_pem} \nPrivate key\n ${tls_private_key.tf_generated_key.private_key_pem}"
+    filename = "${path.module}/aws_secret.txt"
+}
+
+output "public_key" {
+  value = "${tls_private_key.tf_generated_key.public_key_pem}"
+  description = "The TLS genreated public key value"
+}
+
+output "private_key" {
+  value = "${tls_private_key.tf_generated_key.private_key_pem}"
+  description = "The TLS genreated private key value"
+}
+
---
+++ Lab 3/vars.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  default = "ami-0ce21b51cb31a48b8" // Amazon Linux 2 AMI (HVM), SSD Volume Type
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "number_of_instances" {
+    default = 1
+    type = number
+}
+
