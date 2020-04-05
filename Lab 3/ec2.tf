c763a6e5de659e416e4fc95a632967009705034e
Jaxon <mohamedmostels@gmail.com>
2020-04-05 23:36:59 +0200

    allow HTTP

--- Lab 3/ec2.tf
+++ Lab 3/ec2.tf
@@ -91,7 +91,7 @@ resource "aws_security_group" "allow_http_traffic" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${aws_vpc.main_vpc.cidr_block}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
