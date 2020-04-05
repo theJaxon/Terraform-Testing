efe3e79672a3e8eaf86ebd823346fe6e37f44129
Jaxon <mohamedmostels@gmail.com>
2020-04-05 16:26:29 +0200

    jenkins update

--- Lab 3/try.tf
+++ Lab 3/try.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version = "~> 2.56"
-  region     = "${env.AWS_DEFAULT_REGION}"
-  access_key = "${env.AWS_ACCESS_KEY_ID}"
-  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
+  region     = "${AWS_DEFAULT_REGION}"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 
